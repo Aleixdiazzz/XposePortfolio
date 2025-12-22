abbd8de2f28d1136f5f96d433bc4b2bd9670751c
Aleix Díaz <aleixdiazzz@gmail.com>
2025-12-22 08:11:44 +0100

    feat: SSG rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   image: {
     domains: ['minio.xpose.es'],
   },
-  server: {
-    host: true  // ← This tells Astro to listen on 0.0.0.0
-  },
-  integrations: [tailwind()],
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [tailwind()]
 });
